d2a300f73f45681f1b0d88fcc13a587613bed411
Arnav Joshi <arnav1joshi@gmail.com>
2021-12-24 13:35:27 -0500

    Initial Commit

---
+++ SymbolicMath/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SymbolicMath
+//
+//  Created by Arnav Joshi on 12/24/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
